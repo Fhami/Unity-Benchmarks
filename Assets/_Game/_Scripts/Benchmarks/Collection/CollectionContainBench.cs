1f330e8fc97263dee55eeb8f249e52be0e735af5
Khami <napakamee@outlook.com>
2023-09-24 01:27:46 +0700

    Fixed typo

--- Assets/_Game/_Scripts/Benchmarks/Collection/CollectionContainBench.cs
+++ Assets/_Game/_Scripts/Benchmarks/Collection/CollectionContainBench.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class CollectioContainnBench : BenchmarkBase
+public class CollectionContainBench : BenchmarkBase
 {
     private int[] array;
     private List<int> list;
